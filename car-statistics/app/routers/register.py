61613063e4867aff6e08a142e4c3b8fbf8f2ad20
dudar99 <dudaryourko@gmail.com>
2018-09-07 19:53:59 +0300

    update registration view with validators

--- car-statistics/app/routers/register.py
+++ car-statistics/app/routers/register.py
@@ -1,29 +1,41 @@
 from flask import request, redirect, flash
-
-from sqlalchemy.exc import IntegrityError
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from app.models import User
 from app import app
 from app import db
+import json
+import re
 
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/register', methods=['POST'])
 def register():
     """
     GET and POST methods for registration
     :return:
     """
     if request.method == 'POST':
-        try:
-            email = request.form['email']
-            password = request.form['password']
-            new_user = User(email=email, password_plaintext=password)
-            new_user.authenticated = True
-            db.session.add(new_user)
-            db.session.commit()
-            flash('Thanks for registering!', 'success')#if base template has looping through message flash displays
-            return redirect('/')
-        except IntegrityError:
-            db.session.rollback()
+        regemail = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+        email = request.form['email'].strip()
+        if not re.match(regemail, email):
+            return json.dumps({'status': 401,
+                               'message': 'email is`n correct'})
+        if not User.query.filter(User.email == email).first():
+            password = generate_password_hash(request.form['password'])
+            if email and password:
+
+                new_user = User(email=email, password_plaintext=password)
+                db.session.add(new_user)
+                db.session.commit()
+                flash('Thanks for registering!',
+                      'success')  # if base template has looping through message flash displays
+                return redirect('/')
+            else:
+                return json.dumps({'status': 401,
+                                   'message': 'Empty value'})
+        else:
             flash('ERROR! Email ({}) already exists.'.format(email), 'error')
-    return '<h1>Registration form</h1>'
+            return json.dumps({
+                'status': 401,
+                'message': f'email: {email} already exist'
+            })
