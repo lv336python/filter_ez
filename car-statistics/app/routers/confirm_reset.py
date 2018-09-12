9e4817cefaf16a853337591516c4506ecbe79c42
vova1995 <vovapetryna1995@gmail.com>
2018-09-12 12:47:36 +0300

    modify PUT and POST methods in reset password views

--- car-statistics/app/routers/confirm_reset.py
+++ car-statistics/app/routers/confirm_reset.py
@@ -7,25 +7,24 @@ from app import app
 from app import db
 from app.routers.registration.token import confirm_token
 from app.models import User
+from werkzeug.security import generate_password_hash
 
 
 
-@app.route('/api/password_reset/<token>')
+
+@app.route('/api/password_reset/<token>', methods=['PUT'])
 def reset_with_token(token):
     try:
         email = confirm_token(token)
     except ValueError:
         flash('The confirmation link is invalid or has expired.', 'danger')
     data = request.get_json()
-    password = 'blsoaoasgs'
+    password = generate_password_hash(data['password'])
     if password:
         user = User.query.filter(User.email == email).first()
-        print(user)
         if user:
             user.password_plaintext = password
             db.session.add(user)
             db.session.commit()
             return json.dumps({'status': 200, 'token': token}), 200
     return json.dumps({'status': 404, 'message': 'user doesnt exist'}), 404
-
-
--- car-statistics/app/routers/reset_password.py
+++ car-statistics/app/routers/reset_password.py
@@ -1,25 +1,19 @@
-from werkzeug.security import check_password_hash, generate_password_hash
 from flask import request, session, url_for
 from app.routers.registration.token import generate_confirmation_token
 
 import json
-from app import app, db
+from app import app
 from app.models.user import User
 from app.routers.registration.email import send_email
 
 
 
-@app.route("/api/reset", methods=['PUT'])
+@app.route("/api/reset", methods=['POST'])
 def reset_password():
     data = request.get_json()
     email = data['email']
-    # print(session['user_id'])
-    # if session['user_id']:
     if email:
         user = User.query.filter(User.email == email).first()
-        # user.password_plaintext = password
-        # db.session.add(user)
-        # db.session.commit()
         token = generate_confirmation_token(user.email)
         subject = "Password reset requested"
         recover_url = url_for(
