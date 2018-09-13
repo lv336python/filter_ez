13d9c8a963f884d2d4bb86e66d18e4102495175c
underdog18 <oleksandr.proshchuk@gmail.com>
2018-09-13 18:56:54 +0300

    modified fields_definition, generate fields_definition.json file

--- car-statistics/app/services/fileds_definition.py
+++ car-statistics/app/services/fileds_definition.py
@@ -2,23 +2,28 @@
     Module for fields definition
 """
 import pandas as pd
-from collections import defaultdict
+import json
 
 
-df = pd.read_excel('result_test.xls')  # your path to file
-
-cl_names = list(df.columns.values)
-
-
-def fields_define():
+def fields_definition(file_name):
     """ This function defines fields to defaultdict in dict
     to store column names, their values and count of this values in column for statistic
+    :param file_name: parameter for your file name
     :return dict: {'Air bags': {4: 8, 0: 8}, 'Body': {'MPV': 11, 'Sedan': 7}, 'Climate control': {'Yes': 30, 'No': 19}}
     """
+    df = pd.read_excel(file_name)
+
+    cl_names = list(df.columns.values)
+
     field_def = {}
     for cl_name in cl_names:
-        def_dict = defaultdict(int)
-        for val in list(df[cl_name]):
-            def_dict[val] += 1
-            field_def[cl_name] = def_dict
+        cl_name_val = list(df[cl_name])
+        if type(cl_name_val[0]) == str:
+            field_def[cl_name] = list(set(df[cl_name]))
+        else:
+            field_def[cl_name] = dict(min=min(cl_name_val), max=max(cl_name_val))
     return field_def
+
+
+with open('fields_definition.json', 'w') as outfile:
+    json.dump(fields_definition('result_test.xls'), outfile)
