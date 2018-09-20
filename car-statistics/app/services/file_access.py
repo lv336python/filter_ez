202ce39700aea5ce621968e7fefc74c4d5493ac3
Sviatoslav Skotar <sviatolslav.skotar.pz.2016@lpnu.ua>
2018-09-20 18:43:32 +0300

    Change function send_results

--- car-statistics/app/services/file_access.py
+++ car-statistics/app/services/file_access.py
@@ -1,8 +1,9 @@
 import os
 
+from app import app
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-def get_file(user, filename):
-    with open(os.path.join(BASE_DIR, 'files/'+filename), 'rb') as file:
+def get_file(filepath):
+    with open(os.path.join(app.config['UPLOAD_FOLDER'], filepath), 'rb') as file:
         return file.read()
