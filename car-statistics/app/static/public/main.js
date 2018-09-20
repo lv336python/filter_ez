1fe0969f03187737b6d8f9f98c549b5915f0ff26
dudar99 <mkbbkbfast@gmail.com>
2018-09-20 13:29:17 +0300

    code refactor

--- car-statistics/app/static/public/main.js
+++ car-statistics/app/static/public/main.js
@@ -1113,7 +1113,7 @@ Object(_angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__["platformB
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /home/dudar99/Desktop/SS/SSprojects/car-statistics/car-statistics/app/static/src/main.ts */"./src/main.ts");
+module.exports = __webpack_require__(/*! /home/dudar99/Desktop/SS/projects/car-statistics/car-statistics/app/static/src/main.ts */"./src/main.ts");
 
 
 /***/ })
