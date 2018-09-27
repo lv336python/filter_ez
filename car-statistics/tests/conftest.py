ce8e4696a81570f17a8fe178a58fe7ba0d4c7105
dudar99 <dudaryourko@egmail.com>
2018-09-27 22:49:55 +0300

    add simple tests and modify register(FE)

---
+++ car-statistics/tests/conftest.py
@@ -0,0 +1,34 @@
+import json
+import os
+import tempfile
+
+import pytest
+
+from app import app
+
+
+@pytest.fixture
+def client():
+    db_fd, app.config['DATABASE_FAKE'] = tempfile.mkstemp()
+    app.config['TESTING'] = True
+    client = app.test_client()
+
+    yield client
+    os.close(db_fd)
+    os.unlink(app.config['DATABASE_FAKE'])
+
+#
+# def login(client, email, password):
+#     return client.post('/api/login', json=dict(
+#         email=email,
+#         password=password
+#     ), follow_redirects=True)
+#
+#
+# def logout(client):
+#     return client.get('/logout', follow_redirects=True)
+#
+#
+from app.models import User
+
+
