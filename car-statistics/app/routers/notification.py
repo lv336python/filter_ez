141d3680ab7826a20ce64ee004b8a250c3994f77
Sviatoslav Skotar <sviatolslav.skotar.pz.2016@lpnu.ua>
2018-10-02 14:17:40 +0300

    Update user room connection

--- car-statistics/app/routers/notification.py
+++ car-statistics/app/routers/notification.py
@@ -1,21 +1,21 @@
 from flask import session
 from flask_socketio import join_room, leave_room
+from flask_login import current_user
 from app import socketio
 
 
 @socketio.on('connect')
 def on_connect():
-    print('User connected')
+    print(f'{str(current_user)} connected')
 
 
 @socketio.on('join_room')
-def on_join(user_id):
-    print(f'User joined room {user_id}')
-    join_room(user_id)
-    session['room_id'] = user_id
+def on_join(s):
+    print(f'User joined room {current_user}')
+    join_room(current_user.id)
 
 
 @socketio.on('disconnect')
 def on_disconnect():
-    leave_room(session['room_id'])
-    print('Client disconnected')
+    leave_room(current_user.id)
+    print(f'{current_user} disconnected')
