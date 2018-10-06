b79fbc9f2111404b44c4da5d1ab185ad8b8905e9
Sviatoslav Skotar <sviatolslav.skotar.pz.2016@lpnu.ua>
2018-10-06 13:48:36 +0300

    Fix connection when user logged in
    Now login and logout components emit message with the auth status to the main component.
    If the status is not loggedOut then the notification component is initialized and connection with server socket is established.
    Otherwise component is destroyed and ngOnDestroy called, which also destroyes connection with socket.

--- car-statistics/app/routers/notification.py
+++ car-statistics/app/routers/notification.py
@@ -1,4 +1,3 @@
-from flask import session
 from flask_socketio import join_room, leave_room
 from flask_login import current_user
 from app import socketio
@@ -7,11 +6,6 @@ from app import socketio
 @socketio.on('connect')
 def on_connect():
     print(f'{str(current_user)} connected')
-
-
-@socketio.on('join_room')
-def on_join(s):
-    print(f'User joined room {current_user}')
     join_room(current_user.id)
 
 
