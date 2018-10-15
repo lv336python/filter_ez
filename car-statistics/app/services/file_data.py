59156b3c5ceb978056b01483dfc158af939cc7ed
underdog18 <oleksandr.proshchuk@gmail.com>
2018-10-15 18:06:59 +0300

    Interface for DataSet methods

--- car-statistics/app/services/file_data.py
+++ car-statistics/app/services/file_data.py
@@ -1,56 +1,53 @@
 """
-    Module for fields definition
+    Module for fields definition and field statistics
 """
-import pandas as pd
 from collections import defaultdict
-from .filtering import dataframe_actualization
+from app.helper.DataSetPandas import DataSetPandas
 
 
-def mask(df, key, value):
-    return df[df[key] == value]
-
-
-def fields_definition(file_id, user_id, filter=False):
+def fields_definition(filename, filter=None):
     """ This function defines fields to defaultdict in dict
     to store column names, their values and count of this values in column for statistic
-    :param file_name: parameter for your file name
-    :return dict: {'Air bags': {'max': 4, 'min': 0}, 'Body': {'MPV': 11, 'Sedan': 7}, 'Climate control': {'Yes': 30, 'No': 19}}
+    :param file_name: parameter for your filename
+    :param filter: tuple: name of column and value for filter
+    :return dict: {'Air bags': {'max': 4, 'min': 0}, 'Body': ['MPV', 'SUV', [Sedan],
+    'Climate control': ['Yes', 'No']}
     """
-
-    df = dataframe_actualization(file_id, user_id)
-    # df = pd.read_excel(file_name)
+    dataframe = DataSetPandas()
+    dataframe.read(filename)
 
     if filter:
-        pd.DataFrame.mask = mask
-        df = df.mask(*filter)
+        dataframe.filter_set(filter)
 
-
-    cl_names = list(df.columns.values)
+    cl_names = dataframe.get_column_names()
 
     field_def = {}
     for cl_name in cl_names:
-        cl_name_val = list(df[cl_name])
-        if type(cl_name_val[0]) == str:
-            field_def[cl_name] = list(set(df[cl_name]))
+        cl_name_val = dataframe.get_column_values(cl_name)
+        if isinstance(cl_name_val[0], str):
+            field_def[cl_name] = list(set(dataframe.get_column_values(cl_name)))
         else:
             field_def[cl_name] = dict(min=min(cl_name_val), max=max(cl_name_val))
 
     return field_def
 
 
-def fields_statistics(file_name):
-    """ This function defines fields to defaultdict in dict
+def fields_statistics(filename):
+    """This function defines fields to defaultdict in dict
     to store column names, their values and count of this values in column for statistic
     :param file_name: parameter for your file name
-    :return dict: {'Air bags': {4: 8, 0: 8}, 'Body': {'MPV': 11, 'Sedan': 7}, 'Climate control': {'Yes': 30, 'No': 19}}
+    :return dict: {'Air bags': {4: 8, 0: 8}, 'Body': {'MPV': 11, 'Sedan': 7},
+    'Climate control': {'Yes': 30, 'No': 19}}
     """
-    df = pd.read_excel(file_name)
-    cl_names = list(df.columns.values)
+    dataframe = DataSetPandas()
+    dataframe.read(filename)
+
+    cl_names = list(dataframe.get_column_names())
 
     field_def = {}
     for cl_name in cl_names:
         default_dict = defaultdict(int)
-        cl_name_val = list(df[cl_name])
+        cl_name_val = list(dataframe.get_column_values(cl_name))
         for val in cl_name_val:
             if isinstance(val, float):
                 val = round(val, 2)
@@ -66,8 +63,10 @@ def get_data_preview(file_path, number_of_rows=10):
     :param number_of_rows: number of rows to show
     :return:
     """
-    df = pd.read_excel(file_path)
-    columns = list(df.columns)
-    rows = df[df.index < number_of_rows].values.tolist()
+    dataframe = DataSetPandas()
+    dataframe.read(file_path)
+
+    columns = list(dataframe.get_column_names())
+    rows = dataframe.amount_of_rows(number_of_rows)
     return {'columns': columns,
             'rows': rows}
