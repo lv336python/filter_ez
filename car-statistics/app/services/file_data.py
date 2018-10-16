20e2d82aac9229457f01f6bb9b9bc046fdbb0327
Sviatoslav Skotar <sviatolslav.skotar.pz.2016@lpnu.ua>
2018-10-16 22:28:20 +0300

    Statistics and preview works for datasets and not original files

--- car-statistics/app/services/file_data.py
+++ car-statistics/app/services/file_data.py
@@ -4,7 +4,9 @@
 import pandas as pd
 from collections import defaultdict
 from .filtering_service import dataframe_actualization
+from .utils import get_user_file
 
+from app.models import File
 
 def mask(df, key, value):
     return df[df[key] == value]
@@ -38,13 +40,18 @@ def fields_definition(file_id, user_id, filter=False):
     return field_def
 
 
-def fields_statistics(file_name):
+def fields_statistics(dataset):
     """ This function defines fields to defaultdict in dict
     to store column names, their values and count of this values in column for statistic
-    :param file_name: parameter for your file name
+    :param dataset: dataset instance
     :return dict: {'Air bags': {4: 8, 0: 8}, 'Body': {'MPV': 11, 'Sedan': 7}, 'Climate control': {'Yes': 30, 'No': 19}}
     """
-    df = pd.read_excel(file_name)
+    file_path = get_user_file(dataset.file_id, dataset.user_id)  # Exchange with UserFileManager
+    df = pd.read_excel(file_path)
+
+    if dataset.included_rows:
+        df = df.iloc[dataset.included_rows]
+
     cl_names = list(df.columns.values)
 
     field_def = {}
@@ -52,22 +59,23 @@ def fields_statistics(file_name):
         default_dict = defaultdict(int)
         cl_name_val = list(df[cl_name])
         for val in cl_name_val:
-            if isinstance(val, float):
-                val = round(val, 2)
             default_dict[val] += 1
         field_def[cl_name] = default_dict
     return field_def
 
 
-def get_data_preview(file_path, number_of_rows=10):
+def get_data_preview(dataset, number_of_rows):
     """
     Returns dict with names of columns and first 10 or less rows
-    :param file_path: path to excel file to open
+    :param dataset: dataset instance
     :param number_of_rows: number of rows to show
-    :return:
+    :return: dict with list with names of columns and list with lists of values of rows
     """
+    file_path = get_user_file(dataset.file_id, dataset.user_id)  # Exchange with UserFileManager
     df = pd.read_excel(file_path)
-    columns = list(df.columns)
-    rows = df[df.index < number_of_rows].values.tolist()
-    return {'columns': columns,
-            'rows': rows}
+
+    if dataset.included_rows:
+        df = df.iloc[dataset.included_rows]
+
+    return {'columns': list(df.columns),
+            'rows': df[df.index < number_of_rows].values.tolist()}
