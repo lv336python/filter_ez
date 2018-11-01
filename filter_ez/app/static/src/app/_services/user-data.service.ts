dfc83105a00922b7d891ea701386b30b8975ed48
srhstas <sergii.stasiuk@gmail.com>
2018-11-01 12:32:00 +0200

    Remove statistic initialization on component init.

--- filter_ez/app/static/src/app/_services/user-data.service.ts
+++ filter_ez/app/static/src/app/_services/user-data.service.ts
@@ -16,6 +16,9 @@ export class UserDataService {
   public userData: UserData;
   private newUserData = new BehaviorSubject<any>(null);
   castUserData = this.newUserData.asObservable();
+  public statToShow = [];
+  private newStatToShow = new BehaviorSubject<any>([]);
+  castStatToShow = this.newStatToShow.asObservable();
 
   constructor(private http: HttpClient) {
   }
@@ -30,6 +33,14 @@ export class UserDataService {
   onUploadComplete(file) {
     this.userData.userFiles.push(file);
     this.newUserData.next(this.userData);
+  }
+  onAddToStat(datasetId) {
+    this.statToShow.push(datasetId);
+    this.newStatToShow.next(this.statToShow);
+  }
+  onCloseStat(){
+    this.statToShow = [];
+    this.newStatToShow.next(this.statToShow);
   }
 
   private handleError(error: HttpErrorResponse) {
--- filter_ez/app/static/src/app/user-data/user-data-sets/user-data-set/user-data-set.component.ts
+++ filter_ez/app/static/src/app/user-data/user-data-sets/user-data-set/user-data-set.component.ts
@@ -3,6 +3,9 @@ import {DataSet} from '../../../_models/data';
 
 import {UserFileService} from "../../../_services/user-file.service";
 import 'rxjs/Rx' ;
+import {ModalService} from "../../../_services/modal.service";
+import {UserDataService} from "../../../_services/user-data.service";
+
 @Component({
   selector: 'app-user-data-set',
   templateUrl: './user-data-set.component.html',
@@ -10,26 +13,27 @@ import 'rxjs/Rx' ;
 })
 export class UserDataSetComponent implements OnInit {
   @Input() userdataset: DataSet;
-  constructor(private file : UserFileService) { }
-  showDatasetStat: boolean = true;
+
+  constructor(private file: UserFileService,
+              private modalService: ModalService,
+              private dataService: UserDataService) {
+  }
+
 
   downloadDataset(datasetId) {
     let thefile = {};
     this.file.downloadDataset(datasetId).subscribe(
-      data => thefile = new Blob([data], { type: "text/xlsx" } )
+      data => thefile = new Blob([data], {type: "text/xlsx"})
+    );
+  }
 
-    )
-}
 
-  getStatDataset() {
-    if (this.showDatasetStat == true) {
-        this.showDatasetStat = false;
-    }
-    else {
-        this.showDatasetStat = true
-    }
-}
   ngOnInit() {
+  }
+
+  openModal(id: string) {
+    this.modalService.open(id);
+    this.dataService.onAddToStat(this.userdataset.id);
   }
 
 }
