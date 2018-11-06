b3ebf4e4caf5b7f471f66cf6e94e0c18a09739fb
Sviatoslav Skotar <sviatolslav.skotar.pz.2016@lpnu.ua>
2018-11-06 18:11:37 +0200

    Fix service styles and documentation

--- filter_ez/app/services/temp_link_service.py
+++ filter_ez/app/services/temp_link_service.py
@@ -14,8 +14,8 @@ from app.services.utils import temp_file
 def send_to_user(dataset, emails):
     """
     Function send templink or file to user which depends on size of file.
-    :param dataset_id:
-    :param user_id:
+    :param dataset: Dataset instance
+    :param emails: list of strings with emails
     :return: functions sending link or file
     """
     file = temp_file(dataset)
@@ -27,30 +27,30 @@ def send_to_user(dataset, emails):
 def send_templink(path, emails):
     """
     Function generate token and send link to user email.
-    :param path:
-    :param user_id:
-    :return: link
+    :param path: path to file
+    :param emails: list of emails of recepients
+    :return: status
     """
     token = generate_confirmation_token(path)
     subject = "Your file has been processed!"
     recover_url = url_for(
         'index', _external=True) + \
-                  'api/temp_link/' + \
-                  token.decode('utf-8')
+        'api/temp_link/' + \
+        token.decode('utf-8')
     html = f'Your file has been processed successfully. \
             Please download it from link {recover_url}'
     send_email(emails, subject, html)
-    return 'Link send'
+    return 'Link sent'
 
 
 def send_user_file(path, emails):
     """
     Function send file to user MAIL.
-    :param path:
-    :param user_id:
-    :return: file
+    :param path: path to xlsx file to send
+    :param emails: list of recipients emails
+    :return: status
     """
     send_result_to_mail(emails,
                         'result.xlsx',
                         open(path, 'rb').read())
-    return 'File send'
+    return 'File sent'
